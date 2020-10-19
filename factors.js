b6048f7f9e184893cf0db6edb14321ae3cdacbd3
Peter Shank <peter@shank.com>
2020-10-19 23:47:51 +0000

    moved repo to new location

---
+++ factors.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const primes  = require('./primes.js')
+
+function factors (n) {
+    'use strict'
+    const factors = []
+   // const candidates = primes(Math.floor(Math.sqrt(n)))
+   const candidates = [2,3]
+   while ( candidates.length > 0 ) {
+        while ( n % candidates[0] === 0 ) {
+            factors.push(candidates[0])
+            n = n / candidates[0]
+            if ( n === 1 ) {
+                return factors
+            }
+        }
+        candidates.shift()
+        // console.log(candidates)
+    }
+    factors.push(n)
+    // factors = [37]
+    return factors
+}
+
+// module.exports = factors
+console.log(factors(100))
