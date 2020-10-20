96603bbab5250aa8ab94d2d7f025e6aba51c242c
Peter Shank <peter@shank.com>
2020-10-20 17:05:17 +0000

    added factors-test.js

---
+++ factors-test.js
@@ -0,0 +1,6 @@
+const assert = require('assert')
+const factors = require('./factors')
+
+test('should factor an integer greater than 1', () => {
+        assert.deepStrictEqual(factors(30), [2, 3, 5])
+})
