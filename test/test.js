7066551aa63a3fb8b5089ba45712695b4c8486e0
Peter Shank <peter@shank.com>
2020-11-03 19:13:40 +0000

    install mocha

--- test/test.js
+++ test/test.js
@@ -1,51 +1,16 @@
-// a tiny, vanilla testing framework to avoid dependencies.  Taken from:
-// https://www.sohamkamani.com/blog/javascript/making-a-node-js-test-runner/
+const factors = require("../factors.js")
+const assert = require('assert');
 
-// `tests` is a singleton variable that will contain all our tests
-let tests = []
-
-
-// The test function accepts a name and a function
-function test(name, fn) {
-	// it pushes the name and function as an object to
-	// the `tests` array
-	tests.push({ name, fn })
-}
-
-function run() {
-	// `run` runs all the tests in the `tests` array
-	tests.forEach(t => {
-		// For each test, we try to execute the
-		// provided function. 
-		try {
-			t.fn()
-			// If there is no exception
-			// that means it ran correctly
-			console.log('✅', t.name)
-		} catch (e) {
-			// Exceptions, if any, are caught
-			// and the test is considered failed
-			console.log('❌', t.name)
-			// log the stack of the error
-			console.log(e.stack)
-		}
-	})
-}
-
-// Get the list of files from the command line
-// arguments
-const files = process.argv.slice(2)
-
-// expose the test function as a global variable
-global.test = test
-
-// Load each file using `require`
-files.forEach(file => {
-	// Once a file is loaded, it's tests are
-	// added to the `tests` singleton variable
-	require(file)
-})
-
-// Now that all the tests from all the files are
-// added, run them one after the other
-run()
+describe('factors', function() {
+          it('should calculate factors correctly', function() {
+                assert.deepStrictEqual(factors(2), [2]);
+                assert.deepStrictEqual(factors(3), [3]);
+                assert.deepStrictEqual(factors(4), [2, 2]);
+                assert.deepStrictEqual(factors(5), [5]);
+                assert.deepStrictEqual(factors(6), [2, 3]);
+                assert.deepStrictEqual(factors(25), [5, 5]);
+                assert.deepStrictEqual(factors(35), [5, 7]);
+                assert.deepStrictEqual(factors(30), [2, 3, 5]);
+        });
+      });
+      
