01f4110ba6fef2163dbea1ce6ebf5683ebdc5b00
Peter Shank <peter@shank.com>
2020-11-03 18:23:37 +0000

    refactoring test into a folder

--- test/factors-test.js
+++ test/factors-test.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const factors = require('./factors')
+const factors = require('../factors')
 
 test('should factor an integer greater than 1', () => {
         assert.deepStrictEqual(factors(30), [2, 3, 5])
