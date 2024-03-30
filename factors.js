c5d70746363577f5889befdf44d5c71e9d0e1d90
Peter Shank <peter@shank.com>
2024-03-29 20:02:24 -0700

    not sure

--- factors.js
+++ factors.js
@@ -1,25 +1,25 @@
-'use strict'
+"use strict";
 
-const getPrimes  = require('primes')
+const getPrimes = require("primes");
 
-function factors (n) {
-    'use strict'
-    const factors = []
-   const candidates = getPrimes(Math.floor(Math.sqrt(n)))
-   while ( candidates.length > 0 ) {
-        while ( n % candidates[0] === 0 ) {
-            factors.push(candidates[0])
-            n = n / candidates[0]
-            if ( n === 1 ) {
-                return factors
-            }
-        }
-        candidates.shift()
-        // console.log(candidates)
+function factors(n) {
+  "use strict";
+  const factors = [];
+  const candidates = getPrimes(Math.floor(Math.sqrt(n)));
+  while (candidates.length > 0) {
+    while (n % candidates[0] === 0) {
+      factors.push(candidates[0]);
+      n = n / candidates[0];
+      if (n === 1) {
+        return factors;
+      }
     }
-    factors.push(n)
-    // factors = [37]
-    return factors
+    candidates.shift();
+    // console.log(candidates)
+  }
+  factors.push(n);
+  // factors = [37]
+  return factors;
 }
 
-module.exports = factors
+module.exports = factors;
